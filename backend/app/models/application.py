bb68cf6bf421258bb43976d101aeb23ba5500db5
TanishaDutta-106 <tanisha77@usf.edu>
2026-01-05 23:49:52 -0500

    Add create application request schema

---
+++ backend/app/models/application.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+from typing import List
+
+class Application(BaseModel):
+    id: int
+    company: str
+    role: str
+    status: str
+
+class ApplicationList(BaseModel):
+    applications: List[Application]
+
+class CreateApplicationRequest(BaseModel):
+    company: str
+    role: str
+    status: str
